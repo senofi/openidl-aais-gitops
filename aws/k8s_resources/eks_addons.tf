63004b3647f823a7947af7408c2f08ad51f7aecc
Yanko Zhelyazkov <yanko@senofi.ca>
2024-01-12 10:36:14 -0800

    Remove the optional version

--- aws/k8s_resources/eks_addons.tf
+++ aws/k8s_resources/eks_addons.tf
@@ -2,13 +2,11 @@
 resource "aws_eks_addon" "blk_ebs_csi_driver" {
   cluster_name      = local.blk_cluster_name
   addon_name        = "aws-ebs-csi-driver"
-  addon_version     = "v1.26.1-eksbuild.1"
   resolve_conflicts = "PRESERVE"
 }
 
 resource "aws_eks_addon" "app_ebs_csi_driver" {
   cluster_name      = local.app_cluster_name
   addon_name        = "aws-ebs-csi-driver"
-  addon_version     = "v1.26.1-eksbuild.1"
   resolve_conflicts = "PRESERVE"
 }
