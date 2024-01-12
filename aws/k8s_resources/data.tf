87e7fc7e04efaf2f235a2cff728d0d08eed710c7
Yanko Zhelyazkov <yanko@senofi.ca>
2024-01-11 18:51:49 -0800

    Remove obsolete components that are not used and replaced by Ansible scripts

--- aws/k8s_resources/data.tf
+++ aws/k8s_resources/data.tf
@@ -23,41 +23,6 @@ data "terraform_remote_state" "base_setup" {
 #The following code remains same irrespective of backend
 #Reading NLB setup by ingress controller deployed in app EKS
 
-data aws_alb "app_nlb_external" {
-  tags = {
-    "kubernetes.io/cluster/${local.app_cluster_name}" = "owned",
-    "kubernetes.io/service-name" = "nginx-external/nginx-external-ingress-nginx-nginx-external"
-    #"kubernetes.io/service-name" = "ingress-controller/haproxy-ingress-external"
-  }
-  depends_on = [helm_release.app_nginx_external]
-}
-/*
-data aws_alb "app_nlb_internal" {
-  tags = {
-    "kubernetes.io/cluster/${local.app_cluster_name}" = "owned",
-    "kubernetes.io/service-name" = "nginx-internal/nginx-internal-ingress-nginx-nginx-internal"
-    #"kubernetes.io/service-name" = "ingress-controller/haproxy-ingress-internal"
-  }
-  depends_on = [helm_release.app_nginx_internal]
-}*/
-#Reading NLB setup by ingress controller deployed in blk EKS
-data aws_alb "blk_nlb_external" {
-  tags = {
-    "kubernetes.io/cluster/${local.blk_cluster_name}" = "owned",
-    "kubernetes.io/service-name" = "nginx-external/nginx-external-ingress-nginx-nginx-external"
-    #"kubernetes.io/service-name" = "ingress-controller/haproxy-ingress-external"
-  }
-  depends_on = [helm_release.blk_nginx_external]
-}
-/*
-data aws_alb "blk_nlb_internal" {
-  tags = {
-    "kubernetes.io/cluster/${local.blk_cluster_name}" = "owned",
-    "kubernetes.io/service-name" = "nginx-internal/nginx-internal-ingress-nginx-nginx-internal"
-    #"kubernetes.io/service-name" = "ingress-controller/haproxy-ingress-internal"
-  }
-  depends_on = [helm_release.blk_nginx_internal]
-}*/
 #Reading application cluster info
 data "aws_eks_cluster" "app_eks_cluster" {
   name = data.terraform_remote_state.base_setup.outputs.app_cluster_name
